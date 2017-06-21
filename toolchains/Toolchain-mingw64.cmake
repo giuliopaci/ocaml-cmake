c2fffa51aec589eeeeecb283eaa2803bba2b7bff
Giulio Paci <giuliopaci@gmail.com>
2017-06-21 02:01:22 +0200

    Enable tests using wine in Toolchain-mingw64.cmake.

--- toolchains/Toolchain-mingw64.cmake
+++ toolchains/Toolchain-mingw64.cmake
@@ -99,3 +99,10 @@ set(ENV{PKG_CONFIG_PATH} ${CMAKE_FIND_ROOT_PATH}/lib/pkgconfig)
 
 set(INSTALL_PREFIX ${CMAKE_FIND_ROOT_PATH})
 set(ENV{MINGDIR} ${CMAKE_FIND_ROOT_PATH})
+
+
+#-----------------------------------------------------------------------------#
+#                               Testing                                       #
+#-----------------------------------------------------------------------------#
+
+set(TARGET_SYSTEM_EMULATOR wine)
