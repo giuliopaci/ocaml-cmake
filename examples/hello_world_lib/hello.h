fcd3cb4779e6274e170a4e5ad62e4d3d36d32ade
Giulio Paci <giuliopaci@gmail.com>
2017-06-23 11:57:11 +0200

    Enforce C-linkage on hello_world() in hello.h.

--- examples/hello_world_lib/hello.h
+++ examples/hello_world_lib/hello.h
@@ -1,1 +1,20 @@
+#ifndef _HELLO_H_
+#define _HELLO_H_
+
+#undef  HELLO_BEGIN_C_DECLS
+#undef  HELLO_END_C_DECLS
+#ifdef __cplusplus
+#  define HELLO_BEGIN_C_DECLS extern "C" {
+#  define HELLO_END_C_DECLS }
+#else /* !__cplusplus */
+#  define HELLO_BEGIN_C_DECLS
+#  define HELLO_END_C_DECLS
+#endif  /* __cplusplus */
+
+HELLO_BEGIN_C_DECLS
+
 void hello_world();
+
+HELLO_END_C_DECLS
+
+#endif
