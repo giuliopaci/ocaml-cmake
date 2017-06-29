8e91f8ca3358b58aa275e29b1855ecb57520a991
Giulio Paci <giuliopaci@gmail.com>
2017-06-29 16:53:04 +0200

    Added visibility code for DLL on Windows in hello.h.

--- examples/hello_world_lib/hello.h
+++ examples/hello_world_lib/hello.h
@@ -11,9 +11,37 @@
 #  define HELLO_END_C_DECLS
 #endif  /* __cplusplus */
 
+#if defined _WIN32 || defined __CYGWIN__
+#  define HELLO_HELPER_DLL_IMPORT __declspec(dllimport)
+#  define HELLO_HELPER_DLL_EXPORT __declspec(dllexport)
+#  define HELLO_HELPER_DLL_LOCAL
+#else
+#  if __GNUC__ >= 4
+#    define HELLO_HELPER_DLL_IMPORT __attribute__ ((visibility("default")))
+#    define HELLO_HELPER_DLL_EXPORT __attribute__ ((visibility("default")))
+#    define HELLO_HELPER_DLL_LOCAL  __attribute__ ((visibility("hidden")))
+#  else
+#    define HELLO_HELPER_DLL_IMPORT
+#    define HELLO_HELPER_DLL_EXPORT
+#    define HELLO_HELPER_DLL_LOCAL
+#  endif
+#endif
+
+#ifdef HELLO_LIBRARIES_EXPORTS
+#  ifdef HELLO_SRC
+#    define HELLO_API HELLO_HELPER_DLL_EXPORT
+#  else
+#    define HELLO_API HELLO_HELPER_DLL_IMPORT
+#  endif
+#  define HELLO_LOCAL HELLO_HELPER_DLL_LOCAL
+#else
+#  define HELLO_API
+#  define HELLO_LOCAL
+#endif
+
 HELLO_BEGIN_C_DECLS
 
-void hello_world();
+HELLO_API void hello_world();
 
 HELLO_END_C_DECLS
 
